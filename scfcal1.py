6ac9fcbd2b2d98eb94df68b7aaa873566ed12895
Ridlo W. Wibowo <ridlo.w.wibowo@gmail.com>
2013-05-11 13:00:34 +0900

    change commands line

--- scfcal1.py
+++ scfcal1.py
@@ -1,61 +1,50 @@
 #!/usr/bin/python -tt
-import sys, math, os, shutil           
+import sys
+import math
+import os
+import shutil           
 
-# default filename of input parameters:
-ifile = 'ifile_scf_opt.data'
 
-# default headname
-case = 'scf_tmp'
+def scf_change(ifile, case, opt):
+    # check existence of ifile
+    try:
+        os.path.isfile(ifile)
+    except:
+        print sys.argv[0], "ERROR: no input file"
+        sys.exit(1)
+    
+    option = opt
+    # check pwd
+    pwd = os.getcwd()
 
-# if any change
-while len(sys.argv) > 1:
-    option = sys.argv[1]; del sys.argv[1]
-    if option == '-ifile':
-        ifile = sys.argv[1]; del sys.argv[1]
-    elif option == '-case':
-        case = sys.argv[1]; del sys.argv[1]
-    else:
-        print sys.argv[0], ': invalid option', option
+    # read ifile
+    f = open(ifile, 'r')
+    ifile1 = f.readline()               # file_names.data
+    ifile2 = f.readline()               # input_*.data
+    par = f.readline().strip().split()  # parameter 
+
+    try:
+        var = par[0]
+        mi = float(par[1])
+        mx = float(par[2])
+    except:
+        print "ERROR: 3th line in ", ifile
         sys.exit(1)
 
-# check existence of ifile
-try:
-    os.path.isfile(ifile)
-except:
-    print sys.argv[0], " no input file"
-    sys.exit(1)
-
-# check pwd
-pwd = os.getcwd()
-
-# read ifile
-f = open(ifile, 'r')
-ifile1 = f.readline()               # file_names.data
-ifile2 = f.readline()               # input_*.data
-par = f.readline().strip().split()  # parameter 
-
-try:
-    var = par[0]
-    mi = float(par[1])
-    mx = float(par[2])
-except:
-    print "Error: 3th line in ", ifile
-    sys.exit(1)
-
-after = f.readline()
-value = mi
-while value <= mx:
-    # create subdirectory
-    d = case+'_%s_%g' % (var, value)    # name of subdir
-    if os.path.isdir(d):                # does d exist?
-        shutil.rmtree(d)                # yes, then remove d
-        print 'remove directory ', d
+    after = f.readline()
+    value = mi
+    while value <= mx:
+        # create subdirectory
+        d = case+'_%s_%g' % (var, value)    # name of subdir
+        if os.path.isdir(d):                # does d exist?
+            shutil.rmtree(d)                # yes, then remove d
+            print 'remove directory ', d
     
-    os.mkdir(d)                         # make new subdir d
-    print 'make new directory ', d
-    os.chdir(d)                         # change directory d
-    print 'open directory ', d
-    print 'we are in ', os.getcwd()
+        os.mkdir(d)                         # make new subdir d
+        print 'make new directory ', d
+        os.chdir(d)                         # change directory d
+        print 'open directory ', d
+        print 'we are in ', os.getcwd()
 
     shutil.copy('../'+ifile1, './')     # copy file to new dir
     shutil.copy('../'+ifile2, './')
@@ -72,34 +61,29 @@ while value <= mx:
 
 
 
-f.close()
+    f.close()
 
-"""
-# create a subdirectory
-d = case                    # name of subdirectory
-if os.path.isdir(d):         # does d exist?
-    shutil.rmtree(d)        # yes, remove directory d
-    print 'remove directory'
 
-os.mkdir(d)                 # make new directory d
-print 'make new directory'
 
-os.chdir(d)                 # move to new directory d
-print 'change directory to /'+case
+if __name__ == '__name__':
+    
+    # default filename of input parameters:
+    ifile = 'ifile_scf_opt.data'
+    
+    # default headname
+    case = 'scf_tmp'
+    
+    # commands line input
+    while len(sys.argv) > 1:
+        option = sys.argv[1]; del sys.argv[1]
+        if option == '-ifile':
+            ifile = sys.argv[1]; del sys.argv[1]
+        elif option == '-case':
+            case = sys.argv[1]; del sys.argv[1]
+        else:
+            print sys.argv[0], ': invalid option', option
+            print 'USAGE: ', sys.argv[0], ' [option] [input]'
+            sys.exit(1)
 
-# make a copy of input file in this new directory d
-shutil.copy('../'+ifile1, './')
-shutil.copy('../'+ifile2, './')
 
-# change ifile1
-f = open(ifile1, 'w')
-f.write("""
-#%(lv)g
-#%(case)s
-""" % vars())
-f.close()
 
-# change ifile2
-
-print 'finish...'
-"""
