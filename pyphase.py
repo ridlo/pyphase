aaa43f87136eb7830dfe5525d85ec97ff39acc94
Ridlo W. Wibowo <ridlo.w.wibowo@gmail.com>
2013-05-13 17:51:18 +0900

    fix bugs in pyphase..

--- pyphase.py
+++ pyphase.py
@@ -15,16 +15,11 @@ def pyphase(ifile):
     params = scf_clone.read_inputfile(ifile)
     cmd = 'python scf_clone.py ' + ifile
     print 'running :', cmd
-    failure, output = commands.getstatusoutput(cmd)
-    if failure:
-        print 'ERROR in running %s\n%s' % (cmd, output); sys.exit(1)
+    os.system(cmd)
 
     cmd = 'python lattice_opt.py -d '+params[2]+' -m '+params[4]+' '+params[5]+' '+params[6]+' -ifile nfefn.data -ofile lattice_opt.txt -fit q -plot'
     print 'running: ', cmd
-    failure, output = commands.getstatusouput(cmd)
-    if failure:
-        print 'ERROR in running %s\n%s' % (cmd, output); sys.exit(1)
-
+    os.system(cmd)
 if __name__ == '__main__':
     if len(sys.argv) > 1:
             pyphase(sys.argv[1])
